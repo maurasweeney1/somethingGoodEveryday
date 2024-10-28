1086d8f308ae8edf9c7c7ccb86219b24e54111f3
Maura Sweeney <msweeney@zagmail.gonzaga.edu>
2024-10-27 22:50:56 -0700

    add category and default category

--- something-good-everyday/src/AddPosts.js
+++ something-good-everyday/src/AddPosts.js
@@ -1,120 +1,141 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function AddPostPage({ addNewPost }) {
-    const navigate = useNavigate();
-    const [title, setTitle] = useState("");
-    const [text, setText] = useState("");
-    const [link, setLink] = useState("");
-    const [image, setImage] = useState(null);
-    const [error, setError] = useState("");
+  const navigate = useNavigate();
+  const [category, setCategory] = useState("General");
+  const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
+  const [link, setLink] = useState("");
+  const [image, setImage] = useState(null);
+  const [error, setError] = useState("");
 
-    const handleTitle = (e) => {
-      if (e.target.value.length > 0) {
-        setTitle(e.target.value);
-      }
+  const handleTitle = (e) => {
+    if (e.target.value.length > 0) {
+      setTitle(e.target.value);
+    }
+  };
+
+  const handleText = (e) => {
+    // Character limit = 200
+    if (e.target.value.length <= 200) {
+      setText(e.target.value);
+    }
+  };
+
+  const handleImage = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImage(file);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let error = "";
+
+    if (title.length === 0) {
+      error += "Title is required\n";
+    }
+
+    if (text.length === 0 && image === null && link === "") {
+      error += "Post content is required\n";
+    }
+
+    if (error) {
+      setError(error.trim());
+      return; // exit if there are errors
+    }
+
+    const newPost = {
+      category,
+      title,
+      text,
+      image,
+      link,
     };
 
-    const handleText = (e) => {
-      if (e.target.value.length <= 200) { // Character limit = 200
-        setText(e.target.value);
-      }
-    };
+    addNewPost(newPost);
+    navigate("/");
+  };
 
+  return (
+    <div className="new-post">
+      <h1>Add a New Post</h1>
+      {/* add a post */}
+      <form onSubmit={handleSubmit}>
+        <p>
+          <label>
+            Choose a Category:
+            <select
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+            >
+              <option value="General">General</option>
+              <option value="Quotes">Quotes</option>
+              <option value="Stories">Stories</option>
+              <option value="Advice">Advice</option>
+              <option value="Images">Images</option>
+            </select>
+          </label>
+        </p>
+        <p>
+          <label>
+            <textarea
+              value={title}
+              onChange={handleTitle}
+              rows="2"
+              cols="50"
+              placeholder="Add title..."
+            />
+          </label>
+        </p>
+        <p>
+          <label>
+            Write Your Post (Max 200 characters):
+            <textarea
+              value={text}
+              onChange={handleText}
+              rows="6"
+              cols="50"
+              placeholder="Type your text here..."
+            />
+          </label>
+        </p>
+        <p>
+          <label>
+            Add a Link:
+            <input
+              type="url"
+              value={link}
+              onChange={(e) => setLink(e.target.value)}
+              placeholder="https://example.com"
+            />
+          </label>
+        </p>
+        <p>
+          <label>
+            Add an Image:
+            <input type="file" accept="image/*" onChange={handleImage} />
+          </label>
+        </p>
+        <p>
+          {image && (
+            <img
+              src={image}
+              alt="Preview"
+              style={{ width: "200px", marginTop: "10px" }}
+            />
+          )}
+        </p>
+        <button type="submit">Submit Post</button>
 
-    const handleImage = (e) => {
-      const file = e.target.files[0];
-      if (file) {
-        setImage(file); 
-      }
-    };
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      let error = "";
-      
-      if (title.length === 0) {
-        error += "Title is required\n";
-      }
-      
-      if (text.length === 0 && image === null && link === "") {
-        error += "Post content is required\n";
-      }
-      
-      if (error) {
-        setError(error.trim());
-        return; // exit if there are errors
-      }
-    
-      const newPost = {
-        title,
-        text,
-        image,
-        link,
-      };
-    
-      addNewPost(newPost);
-      navigate("/");
-    };
-    
-
-    return (
-      <div className="new-post">
-        <h1>Add a New Post</h1>
-        {/* add a post */}
-        <form onSubmit={handleSubmit}>
-          <p>
-            <label>
-              <textarea
-                value={title}
-                onChange={handleTitle}
-                rows="2"
-                cols="50"
-                placeholder="Add title..."
-              />
-            </label>
-          </p>
-          <p>
-            <label>
-              Write Your Post (Max 200 characters):
-              <textarea
-                value={text}
-                onChange={handleText}
-                rows="6"
-                cols="50"
-                placeholder="Type your text here..."
-              />
-            </label>
-          </p>
-          <p>
-            <label>
-            Add a Link: 
-              <input
-                type="url"
-                value={link}
-                onChange={(e) => setLink(e.target.value)}
-                placeholder="https://example.com"
-              />
-            </label>
-          </p>
-          <p>
-            <label>
-            Add an Image: 
-              <input
-                type="file"
-                accept="image/*"
-                onChange={handleImage}
-              />
-            </label>
-          </p>
-          <p>
-            {image && <img src={image} alt="Preview" style={{ width: '200px', marginTop: '10px' }} />}
-          </p>
-          <button type="submit">Submit Post</button>
-
-          {error && <p style={{ color: 'red', whiteSpace: 'pre-line' }}>{error}</p>}
-        </form>
-      </div>
-    );
-  }
+        {error && (
+          <p style={{ color: "red", whiteSpace: "pre-line" }}>{error}</p>
+        )}
+      </form>
+    </div>
+  );
+}
 
 export default AddPostPage;
