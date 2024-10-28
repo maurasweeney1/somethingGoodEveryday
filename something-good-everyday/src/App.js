03b075e63260807708fbfd43e83afa1d9293908b
Maura Sweeney <msweeney@zagmail.gonzaga.edu>
2024-10-27 19:10:03 -0700

    dark and light modes

--- something-good-everyday/src/App.js
+++ something-good-everyday/src/App.js
@@ -4,6 +4,24 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import React, { useState } from 'react';
 
 function App() {
+
+  const [lightMode, setLightMode] = useState(true);
+
+  const updateColorTheme = (isLightMode) => {
+    setLightMode(isLightMode);
+    if (isLightMode) {
+      document.body.classList.remove("dark-mode");
+      document.body.classList.add("light-mode");
+      document.querySelector("header").classList.remove("dark-mode");
+      document.querySelector("header").classList.add("light-mode");
+    } else {
+      document.body.classList.remove("light-mode");
+      document.body.classList.add("dark-mode");
+      document.querySelector("header").classList.remove("light-mode");
+      document.querySelector("header").classList.add("dark-mode");
+    }
+  };
+
   return (
     <BrowserRouter>
       <Routes>
@@ -13,6 +31,18 @@ function App() {
             <>
               <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
+                <div>
+                  {/* light/dark mode */}
+                  <span>
+                    Color Theme: {lightMode ? "Light Mode " : "Dark Mode "}
+                  </span>
+                  {/* Button to toggle Light/Dark Mode */}
+                  <button
+                    onClick={() => updateColorTheme(!lightMode)}
+                  >
+                    {lightMode ? "Dark Mode " : "Light Mode "}
+                  </button>
+                </div>
               </header>
             </>
           }
