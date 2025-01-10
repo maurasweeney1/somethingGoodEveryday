051cdb156ed8b86e5bcb8d624418a5e5277b64c2
Maura Sweeney <123509194+maurasweeney1@users.noreply.github.com>
2025-01-09 17:37:48 -0800

    backend connection error

--- backend/server.js
+++ backend/server.js
@@ -51,6 +51,7 @@ app.use(
     origin: [
       "http://localhost:3000",
       "https://somethinggoodeveryday.onrender.com",
+      "https://somethinggoodeveryday-backend.onrender.com",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
@@ -58,6 +59,8 @@ app.use(
 );
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
+
+app.options("*", cors());
 
 // MongoDB Connection
 
